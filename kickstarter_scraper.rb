d9c7fa0de8b4bed03ac860ae52beafb7393977fb
Adam Barton <adam.barton333@gmail.com>
2018-07-26 00:17:17 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,7 @@ def create_project_hash
   #title: project.css("h2.bbcard_name strong a").text
   #project.css("div.project-thumbnail a img").attribute("src").value
   #project.css("p.bbcard_blurb").text
+  #project.css("span.location-name").text
   binding.pry
 end
 
